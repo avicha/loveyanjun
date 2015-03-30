017c44c89c7816b0e2e34b4ec41bc37e27f4d4b7
avicha <avichabc@gmail.com>
2015-03-30 09:48:12 +0800

    3.29我向陈燕君同学表白而做的网页

---
+++ js/app/love.js
@@ -0,0 +1,93 @@
+$(function() {
+    $('.love-section,body,html').height(window.innerHeight);
+    $('.love-container').height(20 * window.innerHeight);
+    $('.rose').width(window.innerWidth).height(window.innerWidth);
+    var loveSectionLength = $('.love-section').size();
+    var swipeNext = function() {
+        var currentSectionIndex = $('.love-section.active').index();
+        var nextSectionIndex = currentSectionIndex + 1;
+        if (nextSectionIndex < loveSectionLength) {
+            $('.love-container').animate({
+                top: -100 * nextSectionIndex + '%'
+            }, 'slow', 'cubic-bezier(.77,.10,.22,1)', function() {
+                $('.love-section.active').removeClass('active');
+                $('.love-section').eq(nextSectionIndex).addClass('active');
+                if (nextSectionIndex === loveSectionLength - 1) {
+                    (function() {
+                        var c = document.getElementsByTagName('canvas')[0];
+                        var a = c.getContext('2d');
+                        var m = Math;
+                        var C = m.cos,
+                            S = m.sin,
+                            P = m.pow,
+                            R = m.random;
+                        c.width = c.height = f = 500;
+                        var h = -250;
+
+                        function p(a, b, c) {
+                            if (c > 60) return [S(a * 7) * (13 + 5 / (.2 + P(b * 4, 4))) - S(b) * 50, b * f + 50, 625 + C(a * 7) * (13 + 5 / (.2 + P(b * 4, 4))) + b * 400, a * 1 - b / 2, a];
+                            A = a * 2 - 1;
+                            B = b * 2 - 1;
+                            if (A * A + B * B < 1) {
+                                if (c > 37) {
+                                    n = (j = c & 1) ? 6 : 4;
+                                    o = .5 / (a + .01) + C(b * 125) * 3 - a * 300;
+                                    w = b * h;
+                                    return [o * C(n) + w * S(n) + j * 610 - 390, o * S(n) - w * C(n) + 550 - j * 350, 1180 + C(B + A) * 99 - j * 300, .4 - a * .1 + P(1 - B * B, -h * 6) * .15 - a * b * .4 + C(a + b) / 5 + P(C((o * (a + 1) + (B > 0 ? w : -w)) / 25), 30) * .1 * (1 - B * B), o / 1e3 + .7 - o * w * 3e-6]
+                                }
+                                if (c > 32) {
+                                    c = c * 1.16 - .15;
+                                    o = a * 45 - 20;
+                                    w = b * b * h;
+                                    z = o * S(c) + w * C(c) + 620;
+                                    return [o * C(c) - w * S(c), 28 + C(B * .5) * 99 - b * b * b * 60 - z / 2 - h, z, (b * b * .3 + P((1 - (A * A)), 7) * .15 + .3) * b, b * .7]
+                                }
+                                o = A * (2 - b) * (80 - c * 2);
+                                w = 99 - C(A) * 120 - C(b) * (-h - c * 4.9) + C(P(1 - b, 7)) * 50 + c * 2;
+                                z = o * S(c) + w * C(c) + 700;
+                                return [o * C(c) - w * S(c), B * 99 - C(P(b, 7)) * 50 - c / 3 - z / 1.35 + 450, z, (1 - b / 1.2) * .9 + a * .1, P((1 - b), 20) / 4 + .05]
+                            }
+                        }
+                        setInterval(function() {
+                            for (var i = 0; i < 1e4; i++) {
+                                var s = p(R(), R(), i % 46 / .74)
+                                if (s) {
+                                    var z = s[2];
+                                    var x = ~~(s[0] * f / z - h);
+                                    var y = ~~(s[1] * f / z - h);
+                                    var q = y * f + x;
+                                    if (!m[q] | m[q] > z) {
+                                        m[q] = z;
+                                        a.fillStyle = "rgb(" + ~(s[3] * h) + "," + ~(s[4] * h) + "," + ~(s[3] * s[3] * -80) + ")";
+                                        a.fillRect(x, y, 1, 1);
+                                    }
+                                }
+                            }
+                        }, 1000 / 60);
+                    })();
+                }
+            });
+        }
+    };
+    var swipePrev = function() {
+        var currentSectionIndex = $('.love-section.active').index();
+        var prevSectionIndex = currentSectionIndex - 1;
+        if (prevSectionIndex >= 0) {
+            $('.love-container').animate({
+                top: -100 * prevSectionIndex + '%'
+            }, 'slow', 'cubic-bezier(.77,.10,.22,1)', function() {
+                $('.love-section.active').removeClass('active');
+                $('.love-section').eq(prevSectionIndex).addClass('active');
+            });
+        }
+    };
+    $('.love-section').on('swipeUp', function() {
+        swipeNext();
+    });
+    $('.love-section').on('swipeDown', function() {
+        swipePrev();
+    });
+    $('.p19-love').on('tap', function() {
+        swipeNext();
+    });
+});
